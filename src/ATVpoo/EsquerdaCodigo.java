a0c1c97f99790bcff70e1245bf05fd888a0e7a92
saulo <saulo679@gmail.com>
2021-07-27 16:50:56 -0300

    teste2

---
+++ src/ATVpoo/EsquerdaCodigo.java
@@ -0,0 +1,11 @@
+package ATVpoo;
+
+public class EsquerdaCodigo {
+    public static void main(String[] args) {
+        EsquerdaMalFeito e = new EsquerdaMalFeito();
+        e.Entrada();
+        e.ToNumber();
+        e.CalculoDirecao();
+        e.Saida();
+    }
+}
